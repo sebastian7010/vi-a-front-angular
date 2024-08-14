1b8dd0b6d5ba13af32d16cb66cc0c4d0b08afc62
sebastian7010 <sebastianssc01@gmail.com>
2024-08-14 18:47:48 -0500

    Tu mensaje de commit aquí

---
+++ front-viña/src/app/components/shared/home/card2/card2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card2',
+  standalone: true,
+  imports: [],
+  templateUrl: './card2.component.html',
+  styleUrl: './card2.component.css'
+})
+export class Card2Component {
+
+}
---
+++ front-viña/src/app/components/shared/home/card3/card3.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card3',
+  standalone: true,
+  imports: [],
+  templateUrl: './card3.component.html',
+  styleUrl: './card3.component.css'
+})
+export class Card3Component {
+
+}
